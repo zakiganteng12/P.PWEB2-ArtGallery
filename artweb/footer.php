c2c5bd4ff7f504a6f950e91707969a7054bd2958
Muhammad Dzaky Hasyim <muhammadzakiplg@gmail.com>
2025-02-17 23:45:59 +0700

    Add files via upload

---
+++ artweb/footer.php
@@ -0,0 +1,3 @@
+<footer class="text-dark fs-6 fw-normal text-center p-3 border-top border-dark bg-light" style="--bs-border-opacity: .2;">
+    <p>&copy; <?php echo date("Y"); ?> Your Website. All rights reserved.</p>
+</footer>
